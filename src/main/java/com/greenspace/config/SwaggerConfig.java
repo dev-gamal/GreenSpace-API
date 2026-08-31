1f52cac27f153d28790af901c584c965580582cb
Gamal Badie <149597937+bd-gamal@users.noreply.github.com>
2026-08-31 09:53:29 +0100

    feat: Add Swagger configuration for API documentation and update security settings

---
+++ src/main/java/com/greenspace/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.greenspace.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API GreenSpace")
+                        .version("1.0")
+                        .description("Endpoints documentation for the GreenSpace platform.")
+                        .contact(new Contact().name("GreenSpace Team")))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
